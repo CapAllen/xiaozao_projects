684db6eea21a7b0755a79690d7bb718ad440bcfc
CapAllen <allenjsj@163.com>
2019-12-04 15:42:25 +0800

    p3

---
+++ p3/Chi_Square_Bins.py
@@ -0,0 +1,73 @@
+#卡方分箱
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2
+from pandas import DataFrame,Series
+import scipy
+
+def chi3(arr):
+    '''
+    计算卡方值
+    arr:频数统计表,二维numpy数组。
+    '''
+    assert(arr.ndim==2)
+    #计算每行总频数
+    R_N = arr.sum(axis=1)
+    #每列总频数
+    C_N = arr.sum(axis=0)
+    #总频数
+    N = arr.sum()
+    # 计算期望频数 C_i * R_j / N。
+    E = np.ones(arr.shape)* C_N / N
+    E = E * R_N.reshape(-1,1)
+    square = (arr-E)**2 / E
+    #期望频数为0时，做除数没有意义，不计入卡方值
+    square[E==0] = 0
+    #卡方值
+    v = square.sum()
+    return v
+
+def chiMerge(df,col,target,max_groups=None,threshold=None): 
+    '''
+    卡方分箱
+    df: pandas dataframe数据集
+    col: 需要分箱的变量名（数值型）
+    target: 类标签
+    max_groups: 最大分组数。
+    threshold: 卡方阈值，如果未指定max_groups，默认使用置信度95%设置threshold。
+    return: 包括各组的起始值的列表.
+    '''
+    freq_tab = pd.crosstab(df[col],df[target])
+    #转成numpy数组用于计算。
+    freq = freq_tab.values
+    #初始分组切分点，每个变量值都是切分点。每组中只包含一个变量值.
+    #分组区间是左闭右开的，如cutoffs = [1,2,3]，则表示区间 [1,2) , [2,3) ,[3,3+)。
+    cutoffs = freq_tab.index.values
+    #如果没有指定最大分组
+    if max_groups is None:     
+        #如果没有指定卡方阈值，就以95%的置信度（自由度为类数目-1）设定阈值。
+        if threshold is None:
+            #类数目
+            cls_num = freq.shape[-1]
+            threshold = chi2.isf(0.05,df= cls_num - 1)
+    while True:
+        minvalue = None
+        minidx = None
+        #从第1组开始，依次取两组计算卡方值，并判断是否小于当前最小的卡方
+        for i in range(len(freq) - 1):
+            v = chi3(freq[i:i+2])
+            if minvalue is None or (minvalue > v): #小于当前最小卡方，更新最小值
+                minvalue = v
+                minidx = i
+        #如果最小卡方值小于阈值，则合并最小卡方值的相邻两组，并继续循环
+        if  (max_groups is not None and  max_groups< len(freq) ) or (threshold is not None and minvalue < threshold):
+            #minidx后一行合并到minidx
+            tmp  = freq[minidx] + freq[minidx+1]
+            freq[minidx] = tmp
+            #删除minidx后一行
+            freq = np.delete(freq,minidx+1,0)
+            #删除对应的切分点
+            cutoffs = np.delete(cutoffs,minidx+1,0)
+        else: #最小卡方值不小于阈值，停止合并。
+            break
+    return cutoffs
